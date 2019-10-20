a45fcaa3f31f7f6e3b9628b8ba8595e9e10041da
Harriet Ryder <harriethryder@gmail.com>
2019-10-20 16:19:31 +0100

    Collect user repsonse in a separate channel

--- quiz/main.go
+++ quiz/main.go
@@ -6,43 +6,77 @@ import (
 	"fmt"
 	"flag"
 	"time"
+	"strings"
+	"bufio"
 )
 
 func main() {
+	// Parse the flags
 	fname := flag.String("f", "quiz.csv", "The filename of the quiz CSV file you wish to use")
 	timeLimit := flag.Int("t", 30, "The time limit for the quiz in seconds (default of 30 seconds)")
 	flag.Parse()
-	file, err := os.Open(*fname)
 
+	// Read the CSV file
+	file, err := os.Open(*fname)
 	if err != nil {
 		fmt.Printf("Cannot find file %s\n", *fname)
 		os.Exit(1)
 	}
 
+	in := bufio.NewReader(os.Stdin)
+	
 	r := csv.NewReader(file)
-	questions, _ := r.ReadAll()
+	lines, _ := r.ReadAll()
 	score := 0
 
 	fmt.Println("Are you ready to play? Press enter to continue.")
-	var keyPressed string
-	fmt.Scanln(&keyPressed)
+	in.ReadString('\n')
 
 	time.AfterFunc(time.Duration(*timeLimit) * time.Second, func () {
 		fmt.Println("Time up, sorry!")
-		fmt.Printf("Your total score is %d out of %d\n", score, len(questions))
+		fmt.Printf("Your total score is %d out of %d\n", score, len(lines))
 		os.Exit(1)
 	})
 
-	for _, line := range questions {
-		q := line[0]
-		a := line[1]
-		fmt.Println(q)
-		var userResponse string
-		fmt.Scan(&userResponse)
-		if userResponse == a {
-			score++
+	problems := parseLines(lines)
+
+	for _, p := range problems {
+		fmt.Println(p.question)
+
+		// Collect answer in a separate answer channel
+		answerCh := make(chan string)
+		go func() {
+			answer, err := in.ReadString('\n')
+			if err != nil {
+				fmt.Println(err)
+			}
+			answerCh <- strings.TrimSpace(answer)
+		}()
+
+		select {
+			case answer := <-answerCh:
+				if answer == p.answer {
+					score++
+				}
 		}
 	}
 
-	fmt.Printf("Your total score is %d out of %d\n", score, len(questions))
+	fmt.Printf("Your total score is %d out of %d\n", score, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	result := make([]problem, len(lines))
+	for i, line := range lines {
+		result[i] = problem{
+			question: line[0],
+			answer: strings.TrimSpace(line[1]),
+		}
+	}
+
+	return result
+}
+
+type problem struct {
+	question string
+	answer string
 }
