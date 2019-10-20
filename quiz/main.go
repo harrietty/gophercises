b73a73e30f376da220a3d15d599e8cd30986ea93
Harriet Ryder <harriethryder@gmail.com>
2019-10-20 15:42:04 +0100

    Add confirmation to start

--- quiz/main.go
+++ quiz/main.go
@@ -12,10 +12,20 @@ func main() {
 	fname := flag.String("f", "quiz.csv", "The filename of the quiz CSV file you wish to use")
 	timeLimit := flag.Int("t", 30, "The time limit for the quiz in seconds (default of 30 seconds)")
 	flag.Parse()
-	file, _ := os.Open(*fname)
+	file, err := os.Open(*fname)
+
+	if err != nil {
+		fmt.Printf("Cannot find file %s\n", *fname)
+		os.Exit(1)
+	}
+
 	r := csv.NewReader(file)
 	questions, _ := r.ReadAll()
 	score := 0
+
+	fmt.Println("Are you ready to play? Press enter to continue.")
+	var keyPressed string
+	fmt.Scanln(&keyPressed)
 
 	time.AfterFunc(time.Duration(*timeLimit) * time.Second, func () {
 		fmt.Println("Time up, sorry!")
